845538b15b9bd69ce8293d08d13d79a762c9b708
Bernhard <103026981+Bernhard1966@users.noreply.github.com>
2022-05-18 14:51:29 +0200

    toll deep deeper deepest

--- script_01.js
+++ script_01.js
@@ -4,21 +4,40 @@
 // let arr;
 // arr = ["Ich","bin","Max"];
 // output(arr);
-// output(arr[0]);
+// output(arr[2]);
 // output("------------");
 
-
+//output(arr[1])
 
 /***** Objekte 1 Daten/Funktionen *******/
 
 
+// output(person);
+// output(person.firstName);
+
+// const txt = "ich bin " + person.firstName + " " +
+//                 person.famiylName + person.selary[0] + 
+//                 person.Permission + " p.a .";
+
+// output(txt);
 
 
 /***** Objekte 2 - Hierarchie *******/
 
+const baikal =  {
+                        value: "10m" ,
+                        deep:   {
+                                        deeper:{
+                                                geepest: "das licht ist - 1642m !"
+                                        }
 
 
+                        }
 
+                };
+
+output(baikal.value);
+output(baikal.deep.deeper.geepest);
 
 /** Ausgabe */
 function output(outputData) {
